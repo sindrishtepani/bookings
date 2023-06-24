4c3b62485fa91dc2649d0cdf0cdccdd8315bf143
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-23 20:06:42 -0600

    adding validation forms logic

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/sindrishtepani/bookings/internal/config"
+	"github.com/sindrishtepani/bookings/internal/forms"
 	"github.com/sindrishtepani/bookings/internal/models"
 	"github.com/sindrishtepani/bookings/internal/render"
 )
@@ -49,7 +50,46 @@ func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+}
+
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+	}
 }
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
