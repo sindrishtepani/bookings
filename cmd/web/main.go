3ffbb760ce3da09d8fbbfeead6f3c5dc57e013db
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-26 16:13:49 -0600

    added logger, error and info loggers to main and the app config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/sindrishtepani/bookings/internal/config"
 	"github.com/sindrishtepani/bookings/internal/handlers"
+	"github.com/sindrishtepani/bookings/internal/helpers"
 	"github.com/sindrishtepani/bookings/internal/models"
 	"github.com/sindrishtepani/bookings/internal/render"
 
@@ -19,6 +21,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -44,6 +48,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -66,6 +76,7 @@ func run() error {
 	handlers.NewHandler(repo)
 
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
