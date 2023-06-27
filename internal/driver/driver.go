27d2c439c759e9622356eb4884328c7c30d199b6
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-26 19:45:01 -0600

    using postgres db and writting out internal driver package for postgresql

---
+++ internal/driver/driver.go
@@ -0,0 +1,66 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenConn = 10
+const maxIdleDbConn = 10
+const maxDbLifetime = 5 * time.Minute
+
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenConn)
+	d.SetMaxIdleConns(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+
+	err = testDB(d)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
+
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
