87e83db17b200db88f90427a198443c6cb36531e
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-27 20:33:58 -0600

    slight refactor, changing render template to just Template

--- internal/render/render.go
+++ internal/render/render.go
@@ -20,7 +20,7 @@ var app *config.AppConfig
 var pathToTemplates = "./templates"
 
 // Sets the config from the templates pkg
-func NewTemplates(a *config.AppConfig) {
+func NewRenderer(a *config.AppConfig) {
 	app = a
 }
 
@@ -33,7 +33,7 @@ func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateDa
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) error {
+func Template(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) error {
 	var tc map[string]*template.Template
 	// create a template cache
 	// get the template cache from config
