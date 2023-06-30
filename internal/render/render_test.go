54813089a98da072ec36c6f649a9023b4ea11972
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-29 20:09:19 -0600

    small changes to render and main tests

--- internal/render/render_test.go
+++ internal/render/render_test.go
@@ -25,7 +25,7 @@ func TestAddDefaultData(t *testing.T) {
 	}
 }
 
-func TestRenderTemplate(t *testing.T) {
+func TestTemplate(t *testing.T) {
 	pathToTemplates = "./../../templates"
 
 	tc, err := CreateTemplateCache()
@@ -42,13 +42,13 @@ func TestRenderTemplate(t *testing.T) {
 
 	var ww myWriter
 
-	err = RenderTemplate(&ww, r, "home.page.tmpl", &models.TemplateData{})
+	err = Template(&ww, r, "home.page.tmpl", &models.TemplateData{})
 
 	if err != nil {
 		t.Error("error writing template to browser")
 	}
 
-	err = RenderTemplate(&ww, r, "non-existent.page.tmpl", &models.TemplateData{})
+	err = Template(&ww, r, "non-existent.page.tmpl", &models.TemplateData{})
 
 	if err == nil {
 		t.Error("rendered template that does not exist")
@@ -70,8 +70,8 @@ func getSession() (*http.Request, error) {
 	return r, nil
 }
 
-func TestNewTemplates(t *testing.T) {
-	NewTemplates(app)
+func TestNewRenderer(t *testing.T) {
+	NewRenderer(app)
 }
 
 func TestCreateTemplateCache(t *testing.T) {
