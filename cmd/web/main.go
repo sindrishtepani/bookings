1addf8d552057401a724206fd760b1aca8158afb
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-06-30 13:31:07 -0600

    senting up a send email go routine and adding it to the global app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -33,6 +33,8 @@ func main() {
 	}
 
 	defer db.SQL.Close()
+	defer close(app.MailChan)
+	listenForMail()
 
 	fmt.Print("Starting application on port", portNumber)
 
@@ -50,6 +52,9 @@ func run() (*driver.DB, error) {
 	gob.Register(models.User{})
 	gob.Register(models.Room{})
 	gob.Register(models.Restriction{})
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
 
 	// change this to true when in production
 	app.InProduction = false
