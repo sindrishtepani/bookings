b8ee2c42ec9ed6e4f6028a00fcb97e5198d92b50
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-07-03 15:33:08 -0600

    helper function for getting if auth'ed and passing auth data to template data

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,13 +4,14 @@ import "github.com/sindrishtepani/bookings/internal/forms"
 
 // Holds data sent to templates
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
